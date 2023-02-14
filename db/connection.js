7de521e7424c1e5b2efaeeb0f7b6c3301c9a9839
Ahmed Rahi <arahi@fordham.edu>
2023-02-14 09:53:37 -0500

    revised

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 mongoose.Promise = Promise;
 
@@ -6,9 +6,9 @@ const mongoURI = "mongodb://localhost/monarch-api";
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
-  .then(instance =>
+  .then((instance) =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
-  .catch(err => console.log("Connection Failed.", err));
+  .catch((err) => console.log("Connection Failed.", err));
 
-module.exports = mongoose;
+export default mongoose;
